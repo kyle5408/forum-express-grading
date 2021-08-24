e49bf4fe19bc309d9cc6ef390df5d46b6c688fd1
kyle5408 <kyle5408@yahoo.com.tw>
2021-08-24 11:30:22 +0800

    chore: initialize project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const exhbs = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
@@ -8,4 +12,6 @@ app.listen(port, () => {
 
 require('./routes')(app)
 
+
+//導入自動化測試以後，由於測試環境會用到 app，所以需要在文件最下方輸出 app。
 module.exports = app
