9310c092c42b8cd9132b611da2ab2348851c563b
kyle5408 <kyle5408@yahoo.com.tw>
2021-08-24 20:20:10 +0800

    feat: add authentication and admin mode

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,37 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 const passport = require('passport')
 
+const authenticated = (req, res, next) => {
+  if (req.isAuthenticated()){
+    return next()
+  }
+  res.redirect('/signin')
+}
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    if(req.user.isAdmin) {
+      return next()
+    }
+    return res.redirect('/')
+  }
+  res.redirect('/signin')
+}
+
 module.exports = (app) => {
 
-  app.get('/', (req, res) => {
+  app.get('/', authenticated,(req, res) => {
     res.redirect('/restaurants')
   })
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  app.get('/admin', (req, res) => {
+  app.get('/admin', authenticatedAdmin,(req, res) => {
     res.redirect('/admin/restaurants')
   })
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin/restaurants', authenticatedAdmin,adminController.getRestaurants)
 
-  app.get('/signUp', userController.signUpPage)
-  app.post('/signUp', userController.signUp)
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true}), userController.signIn)
