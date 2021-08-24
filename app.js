9a3ca1b3bdf7cea8a116661c9da4aee0cfb60bf5
kyle5408 <kyle5408@yahoo.com.tw>
2021-08-24 18:27:28 +0800

    feat: add password and email check

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -8,7 +10,14 @@ const port = 3000
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(bodyParser.urlencoded({extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,18 +8,31 @@ const userController = {
     return res.render('signup')
   },
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    })
-    .then(
-      user => {
-        return res.redirect('/signin')
-      }
-    )
+    const { name, email, password, passwordCheck } = req.body
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '確認密碼不相符!')
+      return res.render('signup', { name, email, password, passwordCheck})
+    } else {
+      User.findOne({ where: { email: req.body.email } })
+        .then(user => {
+          if (user) {
+            req.flash('error_messages', '此信箱已被註冊')
+            return res.render('signup', { name, email, password, passwordCheck })
+          } else {
+            User.create({
+              name: req.body.name,
+              email: req.body.email,
+              password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+            })
+              .then(
+                user => {
+                  return res.redirect('/signin')
+                }
+              )
+          }
+        })
+    }
   }
-
 }
 
 module.exports = userController
