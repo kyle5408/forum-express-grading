2b5a9dd992b021e16b6f1b444260b33ab104739b
kyle5408 <kyle5408@yahoo.com.tw>
2021-08-24 12:01:51 +0800

    feat: add restaurant index page

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
 
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由(要放在後面讓前面的設定透過app一起傳過去)
+require('./routes')(app)
 
 //導入自動化測試以後，由於測試環境會用到 app，所以需要在文件最下方輸出 app。
 module.exports = app
