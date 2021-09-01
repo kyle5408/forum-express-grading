f81280b2bae1b875cede293fcdc5449d028027aa
kyle5408 <kyle5408@yahoo.com.tw>
2021-09-01 13:44:54 +0800

    feat: add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,9 +1,15 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
 
 const restController = {
   getRestaurants: (req, res) => {
+    let pageOffset = 0
+    if (req.query.page) {
+      pageOffset = (req.query.page - 1) * pageLimit
+    }
+
     //利用where: { attribute: value } 查詢的方法
     //要傳給 findAll 的參數，需要包裝成物件格式
     const whereQuery = {}
@@ -13,9 +19,16 @@ const restController = {
       whereQuery.categoryId = categoryId
     }
 
-    Restaurant.findAll({ raw: true, nest: true, include: Category, where: whereQuery })
-      .then(restaurants => {
-        const data = restaurants.map(r => ({
+    Restaurant.findAndCountAll({ raw: true, nest: true, include: Category, where: whereQuery, offset: pageOffset, limit: pageLimit })
+      .then(results => {
+        const pageNow = Number(req.query.page) || 1
+        const pages = Math.ceil(results.count / pageLimit)
+        //將頁數轉成陣列傳到views用
+        const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+        const pagePre = (pageNow - 1 < 1) ? pageNow : pageNow - 1
+        //findAndCountAll中有count(資料個數)和rows(資料)
+        const pageNext = (pageNow + 1 > results.count) ? pageNow : pageNow + 1
+        const data = results.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50),
           categoryName: r.Category.name
@@ -25,7 +38,7 @@ const restController = {
           nest: true,
         })
           .then(categories => {
-            return res.render('restaurants', { restaurants: data, categories, categoryId })
+            return res.render('restaurants', { restaurants: data, categories, categoryId, pageNow, totalPage, pagePre, pageNext })
           })
       })
   },
